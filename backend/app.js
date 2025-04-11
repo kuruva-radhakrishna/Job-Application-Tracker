9f315af50f8049fe7b0cd3ed08e777c43365767e
kuruva-radhakrishna <kuruvaradhakrishna77@gmail.com>
2025-04-11 10:52:29 +0530

    changing cors adding vercek domain

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: true,
+  origin:['https://job-application-tracker-kuruva-radhakrishnas-projects.vercel.app'] ,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true, // Allow cookies to be sent
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
